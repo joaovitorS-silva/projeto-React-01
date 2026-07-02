cbf50356d9bc88b192c9cd2d055b76241a366292
joaovitorS-silva <dg>
2026-07-01 21:37:03 -0300

    correcao da tela de tarefa

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -13,7 +13,7 @@ function TaskPage() {
   }
   return (
     <div className=" w-screen min-h-screen bg-slate-500  p-6">
-      <div className="w-[500px] space-y-4">
+      <div className="w-[500px] mx-auto space-y-4">
         <div className="flex   justify-center relative mb-6">
           <button
             onClick={BackPage}
