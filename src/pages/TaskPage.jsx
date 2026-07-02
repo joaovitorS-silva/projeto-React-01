d9e475fbde80f6a986d3ee7ccf93f695b12f92e1
joaovitorS-silva <dg>
2026-07-01 21:54:53 -0300

    erro de responsividade

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -12,9 +12,9 @@ function TaskPage() {
     navigate(-1);
   }
   return (
-    <div className=" flex w-screen min-h-screen bg-slate-500  p-6">
-      <div className="  justify-center w-[500px] mx-auto space-y-4">
-        <div className="flex   justify-center relative mb-6">
+    <div className="w-screen min-h-screen bg-slate-500  p-6">
+      <div className=" w-full  max-w-[500px] mx-auto space-y-4">
+        <div className="   relative mb-6">
           <button
             onClick={BackPage}
             className=" absolute left-0 top-0 bottom-0 text-slate-100"
@@ -25,9 +25,9 @@ function TaskPage() {
             Detalhes da Tarefa
           </h1>
         </div>
-        <div className="bg-slate-200 p-4 rounded-md">
-          <h2 className="text-xl font-bold text-slate-600">{title}</h2>
-          <p className="text-slate-600">{description}</p>
+        <div className=" bg-slate-200 p-4 rounded-md">
+          <h2 className="  text-xl font-bold text-slate-600">{title}</h2>
+          <p className="  text-slate-600">{description}</p>
         </div>
       </div>
     </div>
